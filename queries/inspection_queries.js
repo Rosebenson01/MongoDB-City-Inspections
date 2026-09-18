9c99f813a26a6307f802e3b2187ead42dd828ef1
Rosebenson01 <rosebenson762@gmail.com>
2026-09-18 19:22:45 -0400

    Refactor MongoDB queries
    This commit includes various MongoDB operations such as inserting a document, updating existing documents, and performing queries to count and find records. Additionally, it corrects syntax errors in the queries.

--- queries/inspection_queries.js
+++ queries/inspection_queries.js
@@ -1,1 +1,59 @@
+// Import dataset
+mongoimport --db city --collection inspections --drop ./city_inspections.json
 
+// Verify dataset
+db.inspections.find({"id" : "10021-2015-ENFO"})
+
+db.inspections.find({"result":"Out of Business"},{"business_name":1}).limit(10)
+
+// Insert a Document
+db.inspections.insertone({
+  id: "20032-2020-ACME",
+  certificate_number:"9998888",
+  business_name: "ACME Explosives",
+  date: Date(),
+  result: "Business Padlocked",
+  sector: "Explosive Retail Dealer-999",
+  address: {
+    number: "1721",
+    street: "Boom Road",
+    city: "BRONX"
+    zip: "10463",
+  }
+})
+
+// MongoDB queries
+  // Count the number of unique inspection sectors
+  db. inspections.distinct("sector") .length
+
+  // Compare date fields for AUSTIN 2012 and ACME Explosives
+  db.inspections. Findone ((business-name: "AUSTIN 2022"})}
+
+  db.inspections. findOne({business_name: "ACME Explosives"})
+
+  // Count businesses with "Violation Issued"
+  db.inspections.countDocuments({result: "Violation Issued"})
+
+// Update ACME Document
+db.inspections.updateOne (
+  {id: "20032-2020-ACME"},
+  {
+    $set: {
+      business_name: "New ACME Flowers", 
+      result: "Business Re-opened",
+      comment: "Flowers after the explosion",
+    }
+  }
+)
+
+// Finding updated document
+db. inspections. findOne({id: "20032-2020-ACME"})
+
+// Updating all Documents that contain City is ROSEDALE
+db.inspections.updateMany (
+  {"address.city": "ROSEDALE"},
+  {$set: {"address.zip": "76114"}}
+)
+
+// Delete fisrt document with result: "Violation Issued"
+db.inspections.delete0ne({"result": "Violation Issued"})
